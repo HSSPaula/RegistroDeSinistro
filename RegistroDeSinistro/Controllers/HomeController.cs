6b1b94341e88f377bdb6f1279ebb2055ddaaa10b
Hudson Soares <hudisoares@gmail.com>
2023-08-01 22:46:20 -0300

    Implementação das telas concluída

--- RegistroDeSinistro/Controllers/HomeController.cs
+++ RegistroDeSinistro/Controllers/HomeController.cs
@@ -15,12 +15,34 @@ namespace RegistroDeSinistro.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<Sinistro> model = new List<Sinistro>();
+            model.Add(new Sinistro(1, DateTime.Now, "RouboFurto", "Teste", 1, "Bairro", "Curitiba", "PR", "Brasil"));
+            model.Add(new Sinistro(2, DateTime.Now, "RouboFurto", "Teste", 1, "Bairro", "Curitiba", "PR", "Brasil"));
+            model.Add(new Sinistro(3, DateTime.Now, "RouboFurto", "Teste", 1, "Bairro", "Curitiba", "PR", "Brasil"));
+            model.Add(new Sinistro(4, DateTime.Now, "RouboFurto", "Teste", 1, "Bairro", "Curitiba", "PR", "Brasil"));
+            model.Add(new Sinistro(5, DateTime.Now, "RouboFurto", "Teste", 1, "Bairro", "Curitiba", "PR", "Brasil"));
+            return View(model);
         }
 
-        public IActionResult Privacy()
+        public IActionResult AddSinistro()
         {
-            return View();
+            Sinistro model = new Sinistro();
+            return View(model);
+        }
+        public IActionResult EditSinistro(int id)
+        {
+            Sinistro model = new Sinistro(1, DateTime.Now, "RouboFurto", "Teste", 1, "Bairro", "Curitiba", "PR", "Brasil");
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult RegistrarSinistro(Sinistro model)
+        {
+            return RedirectToAction("Index");
+        }
+         [HttpPost]
+        public IActionResult EditarSinistro(Sinistro model)
+        {
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
